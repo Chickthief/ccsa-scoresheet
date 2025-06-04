e464e4869042926e27c7c68e52badcf9fbf371e3
Chickthief <marcussl.chan@gmail.com>
2025-06-04 11:06:39 -0400

    deleted old/unecessary files

--- src/pregame/components/PlayerString.js
+++
@@ -1,9 +0,0 @@
-export function PlayerString() {
-    return (
-        <div>
-            <p>Paste in more players:</p>
-            <input className="input-group input-group-lg" type='text'/>
-            <button className="button-ccsa" type="button">Submit</button>
-        </div>
-    );
-}
